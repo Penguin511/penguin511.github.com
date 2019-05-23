eda49ee4065bc9601bc9daaab0cbfae01cf9702d
Penguin511 <50557834+Penguin511@users.noreply.github.com>
2019-05-23 18:47:05 -0500

    Update script.js

--- script.js
+++ script.js
@@ -37,7 +37,8 @@ const MASTER_MAP = [
         "items":[
             {"name": "Reddit", "shortcutKey": "z", "url": "https://reddit.com"},
             {"name": "Youtube", "shortcutKey": "x", "url": "https://www.youtube.com/feed/subscriptions"},
-            {"name": "Crossword", "shortcutKey": "c", "url": "https://www.washingtonpost.com/crossword-puzzles/daily/?utm_term=.2560f94cd695"}
+            {"name": "Crossword", "shortcutKey": "c", "url": "https://www.washingtonpost.com/crossword-puzzles/daily/?utm_term=.2560f94cd695"},
+            {"name": "Nautilus", "shortcutKey": "n", "url": "https://nautil.us"}
         ]
     }
 ]
