cf68c3bbdc9173423d92c8a0f32910c0031e96a8
Penguin511 <50557834+Penguin511@users.noreply.github.com>
2019-07-13 21:04:21 -0500

    Update script.js

--- script.js
+++ script.js
@@ -40,7 +40,8 @@ const MASTER_MAP = [
             {"name": "Reddit", "shortcutKey": "z", "url": "https://reddit.com"},
             {"name": "Youtube", "shortcutKey": "x", "url": "https://www.youtube.com/feed/subscriptions"},
             {"name": "Crossword", "shortcutKey": "c", "url": "https://www.washingtonpost.com/crossword-puzzles/daily/?utm_term=.2560f94cd695"},
-            {"name": "Nautilus", "shortcutKey": "n", "url": "https://nautil.us"}
+            {"name": "Nautilus", "shortcutKey": "n", "url": "https://nautil.us"},
+            {"name": "Mastodon", "shortcutKey": "o", "url": "https://fosstodon.org/web/"}
         ]
     }
 ]
