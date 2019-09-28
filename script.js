8720eb1e7043e2397775a640a7071db37e7b947c
Penguin511 <50557834+Penguin511@users.noreply.github.com>
2019-09-28 12:33:04 -0500

    Update script.js

--- script.js
+++ script.js
@@ -46,6 +46,7 @@ const MASTER_MAP = [
             {"name": "Crossword", "shortcutKey": "c", "url": "https://www.washingtonpost.com/crossword-puzzles/daily/?utm_term=.2560f94cd695"},
             {"name": "Nautilus", "shortcutKey": "n", "url": "https://nautil.us"},
             {"name": "Mastodon", "shortcutKey": "o", "url": "https://fosstodon.org/web/"}
+            {"name": "PiHole", "url": "192.168.86.69"}
         ]
     }
 ]
