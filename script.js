35b4862c65be909010cdd98832190fe97ec6234d
Penguin511 <50557834+Penguin511@users.noreply.github.com>
2020-01-21 17:37:05 -0600

    Update script.js

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ const MASTER_MAP = [
             {"name": "Netlify", "shortcutKey": "", "url": "https://app.netlify.com"},
             {"name": "Metaforest", "shortcutKey": "", "url": "https://github.com/Penguin511/metaforest"},
             {"name": "Mainsite", "shortcutKey": "", "url": "https://github.com/Penguin511/mainsite"},
-            {"name": "Nextcloud", "url": "jordanlutter.net"}
+            {"name": "Nextcloud", "url": "https://jordanlutter.net"}
         ]
     },
     {
