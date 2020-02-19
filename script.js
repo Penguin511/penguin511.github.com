983fc94b2234bebf4a14acdd1e30cf89bb5811c7
Penguin511 <50557834+Penguin511@users.noreply.github.com>
2020-02-18 20:23:15 -0600

    Update script.js

--- script.js
+++ script.js
@@ -22,7 +22,10 @@ const MASTER_MAP = [
             {"name": "Messages", "shortcutKey": "m", "url": "https://messages.google.com/web/conversations?redirected=true"},
             {"name": "ToDo", "shortcutKey": "r", "url": "https://www.rememberthemilk.com/app"},
             {"name": "Kanban", "url": "http://192.168.86.132:8080/b/bPuJxrZJpc2W8j35r/to-do"},
-            {"name": "Topline", "shortcutKey": "b", "url": "https://my.toplinecu.com/DashboardV2"}
+            {"name": "Topline", "shortcutKey": "b", "url": "https://my.toplinecu.com/DashboardV2"},
+            {"name": "Torrents", "url": "https://torrents-csv.ml/#/"}
+           
+          
         ]
     },
     {
